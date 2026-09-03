02156b8f3cf098c469d25bec3a2ca3100d65949b
SATURNXVZ <vvitorhenriique@gmail.com>
2026-09-03 20:40:05 -0300

    Delete EtiquetaEnvio.java

--- EtiquetaEnvio.java
+++
@@ -1,7 +0,0 @@
-/**
- * etiqueta de envio que sera gerada para cada pedido
- * cada pais possui sua propria transportadora
- */
-public interface EtiquetaEnvio {
-    String gerarEtiqueta(Pedido pedido);
-}
