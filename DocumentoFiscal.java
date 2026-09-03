71e300e247e20b832e7bd3a2f9f985d1c3533394
SATURNXVZ <vvitorhenriique@gmail.com>
2026-09-03 20:17:08 -0300

    Delete DocumentoFiscal.java

--- DocumentoFiscal.java
+++
@@ -1,7 +0,0 @@
-/**
- * documento fiscal que sera gerado para cada pedido
- * cada pais possui seu proprio formato de documento fiscal
- */
-public interface DocumentoFiscal {
-    String gerarDocumento(Pedido pedido);
-}
