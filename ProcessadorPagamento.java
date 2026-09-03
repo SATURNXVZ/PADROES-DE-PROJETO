062681eb9aad6ec23a85532c0ae7feb46eef83db
SATURNXVZ <vvitorhenriique@gmail.com>
2026-09-03 20:41:08 -0300

    Delete ProcessadorPagamento.java

--- ProcessadorPagamento.java
+++
@@ -1,7 +0,0 @@
-/**
- * processador de pagamento que sera usado para cada pedido
- * cada pais possui seus proprios metodos de pagamento
- */
-public interface ProcessadorPagamento {
-    String processarPagamento(Pedido pedido);
-}
